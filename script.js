278274c8aaab5aa1b5df3fb6a3a4c62824b596f9
Rosa T <rosa.tomalin@gmail.com>
2024-03-01 16:39:54 +0000

    tidied logic

--- script.js
+++ script.js
@@ -1,49 +1,113 @@
 let dummyTodos = [
     {title: "The first todo goes here", id: 1},
-    {title: "Sneaky extra lad", id: 6},
     {title: "The second todo goes here", id: 2, parentId: 1},
     {title: "The third todo goes here", id: 3, parentId: 1},
     {title: "And the fourth goes here", id: 4},
     {title: "AND the fifth goes here", id: 5, parentId: 4},
+    {title: "Sneaky extra lad", id: 6},
     {title: "Who's this now!", id: 7, parentId: 4}
 ]
 
-function makeTodo(todoData) {
-    let todo = document.createElement("li")
-    todo.classList.add("todo")
-    todo.setAttribute("id", todoData.id)
+function makeTodoMainContent() {
     let mainContent = document.createElement("div")
     mainContent.classList.add("todo-main-content")
-    todo.appendChild(mainContent)
+    return mainContent
+}
+
+function makeDragHandle() {
     let dragHandle = document.createElement("img")
     dragHandle.classList.add("drag-handle")
     dragHandle.setAttribute("src", "images/drag-handle-svgrepo-com.svg")
-    mainContent.appendChild(dragHandle)
-    let todoText = document.createElement("div")
-    todoText.classList.add("todo-text")
-    mainContent.appendChild(todoText)
+    // Prevent draghandle being draggable independent of parent element
+    dragHandle.setAttribute("draggable", "false")
+    return dragHandle
+}
+
+function makeCheckbox(todoId) {
     let checkbox = document.createElement("input")
     checkbox.classList.add("todo-checkbox")
     // TODO add check to checked checkbox
-    checkbox.setAttribute("id", "checked")
+    checkbox.setAttribute("id", `${todoId}-checkbox`)
     checkbox.setAttribute("type", "checkbox")
-    todoText.appendChild(checkbox)
-    let textContent = document.createElement("p")
-    textContent.classList.add("text-content")
-    textContent.textContent = todoData.title
-    todoText.appendChild(textContent)
-    return todo
+    return checkbox
 }
 
-function renderTodos(todoList) {
+function makeTextAndCheckboxContainer() {
+    let textAndCheckboxContainer = document.createElement("div")
+    textAndCheckboxContainer.classList.add("todo-text")
+    return textAndCheckboxContainer
+}
+
+function makeTextContent(title) {
+    let textContent = document.createElement("p")
+    textContent.classList.add("text-content")
+    textContent.textContent = title
+    return textContent
+}
+
+function setDraggable(mainContent, id) {
+    mainContent.setAttribute("draggable", "true")
+    mainContent.addEventListener("dragstart", (event) => {
+        event.dataTransfer.setData("text/plain", id)
+    })
+}
+
+function setDropTargetable(todoEl) {
+    todoEl.addEventListener("dragenter", (event) => {
+        event.preventDefault()
+        // Add some styling code here
+    })
+    todoEl.addEventListener("dragover", (event) => {
+        event.preventDefault()
+    })
+    todoEl.addEventListener("drop", (event) => {
+        let childId = event.dataTransfer.getData("text/plain")
+        let parentId = todoEl.id
+        if (childId !== parentId) {
+            let childData = dummyTodos.find((e) => e.id === parseInt(childId))
+            childData.parentId = parentId
+            renderTodos(dummyTodos)
+            event.preventDefault()
+        }
+    })
+}
+
+function makeTodo(todoData) {
+    
+    let { id, title } = todoData
+    
+    let todoEl = document.createElement("li")
+    todoEl.classList.add("todo")
+    todoEl.setAttribute("id", id)
+
+    let mainContent = makeTodoMainContent()
+    setDraggable(mainContent, id)
+    todoEl.appendChild(mainContent)
+    let dragHandle = makeDragHandle()
+    let textAndCheckboxContainer = makeTextAndCheckboxContainer()
+    mainContent.append(dragHandle, textAndCheckboxContainer)
+    let checkbox = makeCheckbox(id)
+    let textContent = makeTextContent(title)
+    textAndCheckboxContainer.append(checkbox, textContent)
+    
+    return todoEl
+}
+
+function renderTodos(todoDataList) {
     let listContainer = document.getElementById("main-todo-list")
-    // Storing subtodos in a list until all primary todos have been appended to the document
+
+    // Empty before rerendering
+    while (listContainer.firstChild) {
+        listContainer.removeChild(listContainer.firstChild);
+    }
+    // Storing subtodos in a list of lists until all primary todos have been appended to the document
     let subtodoLists = [];
-    for (let todoData of todoList) {
+    
+    for (let todoData of todoDataList) {
         let todoEl = makeTodo(todoData);
         if (todoData.parentId) {
             // Get our subtodo list for this task if we already have one
-            let subtodoListEntry = subtodoLists.filter(e => e.parentId === todoData.parentId)[0]
+            let subtodoListEntry = subtodoLists.find(e => e.parentId === todoData.parentId)
             if (!subtodoListEntry) {
                 let subtodoList = document.createElement("ul");
                 subtodoList.classList.add("todo-list", "subtodo-list")
@@ -55,6 +119,8 @@ function renderTodos(todoList) {
             }
             continue;
         }
+        // Setting droptarget here so only top-level tasks are valid dropzones
+        setDropTargetable(todoEl)
         listContainer.appendChild(todoEl)
     }
     // Adding the subtodos
